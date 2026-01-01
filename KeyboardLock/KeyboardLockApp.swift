3c321a07a909e2cbf8d5636b4c45f14ea074c509
martin-buur <martinbuurman@pm.me>
2026-01-01 11:54:51 +0400

    Initial Commit

---
+++ KeyboardLock/KeyboardLockApp.swift
@@ -0,0 +1,32 @@
+//
+//  KeyboardLockApp.swift
+//  KeyboardLock
+//
+//  Created by Martijn Buurman on 01/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KeyboardLockApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ KeyboardLockTests/KeyboardLockTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardLockTests.swift
+//  KeyboardLockTests
+//
+//  Created by Martijn Buurman on 01/01/2026.
+//
+
+import Testing
+@testable import KeyboardLock
+
+struct KeyboardLockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
